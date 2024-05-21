48115f74822128b759df3dbedc10e76175ecd2bf
Edward-BITC <tuyencapvan0201@gmail.com>
2024-05-21 20:41:18 +0700

    anhlong_huongdan

---
+++ src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        String x = "Tuyen xinh trai ";
+
+        System.out.println(x);
+
+
+
+    }
+}
